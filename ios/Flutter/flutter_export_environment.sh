a05eb602c8a6cc594f2630239d8264fdaebb928b
Titir Adhikary <titir.adhikary@hotmail.com>
2019-11-13 18:06:23 +0530

    Changes for release

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/mr-redible/rishav/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/mr-redible/rishav/srijan20/srijan2020-app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\titir\Documents\FlutterLearn\srijan2020-app"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/mr-redible/rishav/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
